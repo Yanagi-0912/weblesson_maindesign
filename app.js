16bdbf6d70b2f3883f03940aea3a42deb4e80426
Jeffery Yu <930912jeffery@gmail.com>
2024-12-17 13:56:46 +0800

    sever

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/weblessonDB')
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error(err));
+
+
+
+const Account = mongoose.model('Account', AccountData);
+
+const AccountData = new mongoose.Schema({
+    account: { type: String, required: true },
+    username: { type: String, required: true },
+    password: { type: String, required: true }
+});
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, () => {
+    console.log(`Server listening on http://localhost:3000`);
+});
